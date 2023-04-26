0a36777240f3d60ab173ce102855656c2d8275d9
michaelafk <onedeath24@gmail.com>
2023-04-26 21:05:38 +0200

    creado teclat

--- app/src/main/java/com/example/practicafinalalgoritmia/MainActivity.java
+++ app/src/main/java/com/example/practicafinalalgoritmia/MainActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     public int MaxWidthDisplay;
     public int TextViewWidth;
     public int TextViewHeight;
+    public int ButtonHeight;
+    public int ButtonWidth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         MaxWidthDisplay = metrics.widthPixels;
         TextViewWidth = (MaxWidthDisplay/longitud) - 10;
         TextViewHeight = (MaxWidthDisplay/longitud) - 10;
+        ButtonHeight = (MaxWidthDisplay/13)-10;
+        ButtonWidth = (MaxWidthDisplay/13)-10;
         crearGraella();
     }
     public void crearGraella(){
@@ -55,6 +60,40 @@ public class MainActivity extends AppCompatActivity {
             }
             X = 0;
         }
+    }
+    public void crearTeclat{
+        ConstraintLayout constraintLayout = findViewById(R.id.layout);
+        char [] character = "abcdefghijklmnopqrstuvwxyzç".toCharArray();
+        Character alfabeto[]= new Character[26];
+        int x = 30;
+        int y = MaxHeightDisplay-2*ButtonHeight;
+        boolean change = true;
+        for(int i = 0; i<alfabeto.length;i++){
+            alfabeto[i] = character[i];
+            if(i >= 13 && change){
+                x = 0;
+                y=MaxHeightDisplay-ButtonHeight;
+                change = false;
+            }
+            x+=ButtonWidth;
+            Button button = CreateButton(x,y,character[i]);
+            constraintLayout.addView(button);
+        }
+    }
+    public Button CreateButton(int X,int Y,Character ch){
+        Button aux = new Button(this);
+        GradientDrawable gd = new GradientDrawable();
+        gd.setCornerRadius(5);
+        gd.setStroke(3, Color.GRAY);
+        String text = ch.toString();
+        aux.setText(text);
+        aux.setBackground(gd);
+        aux.setWidth(ButtonWidth-5);
+        aux.setHeight(ButtonHeight-5);
+        // Posicionar el TextView
+        aux.setX(X);
+        aux.setY(Y);
+        return aux;
     }
     public TextView CreateTextView(int fila, int columna, int X, int Y){
         TextView aux = new TextView(this);
